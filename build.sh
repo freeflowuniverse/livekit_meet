5418a8cb75fb6c0c76fba514e7a62f423b27f432
timurgordon <timurgordon@gmail.com>
2024-10-05 06:55:47 +0200

    fix serving assets and build script

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ cd "$(dirname "$0")"
 # Build the project
 pnpm build
 
+rm -r docs
+mkdir docs
+
 # Move the built files to the docs directory
-mv out/* docs/
+mv -f out/* docs/
 
 # Create a .nojekyll file in the docs directory
 touch docs/.nojekyll
