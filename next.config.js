3b772b88fc1d987382c00f917943a4918b2126a9
timurgordon <timurgordon@gmail.com>
2024-10-08 12:48:05 +0200

    generate with base path

--- next.config.js
+++ next.config.js
@@ -1,22 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.STATIC_URL || 'https://freeflowuniverse.github.io/livekit_meet',
-  output: 'export',
-  swcMinify: false,
-  reactStrictMode: false,
-  productionBrowserSourceMaps: true,
-  plugins: {
-    cssnano: false,
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-    // Important: return the modified config
-    config.module.rules.push({
-      test: /\.mjs$/,
-      enforce: 'pre',
-      use: ['source-map-loader'],
-    });
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+    assetPrefix: 'https://freeflowuniverse.github.io/livekit_meet',
+    basePath: '/meet',
+    output: 'export',
+    swcMinify: false,
+    reactStrictMode: false,
+    productionBrowserSourceMaps: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+      // Important: return the modified config
+      config.module.rules.push({
+        test: /\.mjs$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      });
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
